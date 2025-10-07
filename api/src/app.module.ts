aefc14ff8fed013376dc75228bed9afdc0f0e774
leonardo029 <moreira.leonardo83@outlook.com>
2025-10-07 13:43:20 -0300

    feat: Definição das configurações de conexão com o banco de dados

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigType } from '@nestjs/config';
+import { databaseConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
+    TypeOrmModule.forRootAsync({
+      inject: [databaseConfig.KEY],
+      useFactory: (config: ConfigType<typeof databaseConfig>) => {
+        return config;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
