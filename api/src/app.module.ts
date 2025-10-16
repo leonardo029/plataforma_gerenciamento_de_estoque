b4696e6e74889484902fc222393c8b62ae452945
leonardo029 <moreira.leonardo83@outlook.com>
2025-10-16 15:52:04 -0300

    feat: Atualização do AppModule para utilização dos novos módulos criados e de transações

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,7 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigType } from '@nestjs/config';
 import { databaseConfig } from './config/database.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserModule } from './modules/users/user.module';
+import { UserModule } from './modules/user/user.module';
+import { ContactModule } from './modules/contact/contact.module';
+import { AddressModule } from './modules/address/address.module';
+import { addTransactionalDataSource } from 'typeorm-transactional';
+import { DataSource } from 'typeorm';
+import { StateModule } from './modules/state/state.module';
+import { CityModule } from './modules/city/city.module';
 
 @Module({
   imports: [
@@ -12,8 +18,20 @@ import { UserModule } from './modules/users/user.module';
       useFactory: (config: ConfigType<typeof databaseConfig>) => {
         return config;
       },
+      dataSourceFactory: async (options) => {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+        const ds = new DataSource(options);
+        await ds.initialize();
+        return addTransactionalDataSource(ds);
+      },
     }),
     UserModule,
+    ContactModule,
+    AddressModule,
+    StateModule,
+    CityModule,
   ],
   controllers: [],
   providers: [],
