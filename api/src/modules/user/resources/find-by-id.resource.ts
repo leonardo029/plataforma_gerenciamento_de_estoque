4e5637f3751af7eb0af722b6e768c1d443fbd227
leonardo029 <moreira.leonardo83@outlook.com>
2025-10-16 15:43:17 -0300

    feat: Finalização do módulo 'User'

---
+++ api/src/modules/user/resources/find-by-id.resource.ts
@@ -0,0 +1,33 @@
+import { UserEntity } from '../entities';
+
+export class FindByIdUserResource {
+  [key: string]: any;
+
+  constructor(entity: UserEntity) {
+    this.id = entity.id;
+    this.name = entity.name;
+    this.email = entity.email;
+    this.address = {
+      street: entity.address.street,
+      streetType: entity.address.streetType.name,
+      complement: entity.address.complement,
+      cep: entity.address.cep,
+      number: entity.address.number,
+      neighborhood: entity.address.neighborhood,
+      city: {
+        name: entity.address.city.name,
+        state: {
+          name: entity.address.city.state.name,
+          acronym: entity.address.city.state.acronym,
+          region: entity.address.city.state.region,
+        },
+      },
+    };
+    this.contact = {
+      countryCode: entity.contact.country_code,
+      ddd: entity.contact.ddd,
+      phoneNumber: entity.contact.phone_number,
+    };
+    this.isActivated = entity.isActivated;
+  }
+}
