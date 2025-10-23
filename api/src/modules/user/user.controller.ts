af70fb8748cf4457864b5063c82dfccdb6bad8dc
leonardo029 <moreira.leonardo83@outlook.com>
2025-10-23 11:39:59 -0300

    feat: Adicionando validador de roles nos endpoints create, patch e delete do módulo user

--- api/src/modules/user/user.controller.ts
+++ api/src/modules/user/user.controller.ts
@@ -8,29 +8,33 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { IsPublic, Roles } from '../auth/decorators';
 import { CreateUserDto, UpdateUserDto } from './dto';
 import { UserRoleType } from './types';
 import { UserService } from './user.service';
+import { RolesGuard } from '../auth/guards';
 
 @Controller('user')
 export class UserController {
   @Inject(UserService)
   private readonly userService: UserService;
 
+  @UseGuards(RolesGuard)
+  @Roles(UserRoleType.ADMIN)
   @IsPublic()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
-  @Roles(UserRoleType.ADMIN)
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @UseGuards(RolesGuard)
   @Roles(UserRoleType.ADMIN)
   @Patch(':id')
   update(
@@ -40,6 +44,8 @@ export class UserController {
     return this.userService.update(id, updateUserDto);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles(UserRoleType.ADMIN)
   @Delete(':id')
   delete(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
     return this.userService.delete(id);
