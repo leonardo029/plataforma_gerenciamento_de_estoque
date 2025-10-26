1e1032b8231e274074359404732c51d497cb2cfc
Leonardo Moreira <moreira.leonardo83@outlook.com>
2025-10-26 16:34:43 -0300

    feat: Menu vertical e horizontal

--- app-client/src/layouts/default.vue
+++ app-client/src/layouts/default.vue
@@ -1,4 +1,20 @@
 <template>
+  <v-navigation-drawer v-model="drawer" width="280">
+    <v-list density="comfortable" nav>
+      <v-list-item
+        to="/dashboard"
+        title="Dashboard"
+        prepend-icon="mdi-view-dashboard"
+      />
+      <!-- Adicione mais itens de menu aqui conforme criarmos novas telas -->
+    </v-list>
+  </v-navigation-drawer>
+
+  <v-app-bar>
+    <v-app-bar-nav-icon @click="drawer = !drawer" />
+    <v-app-bar-title>Application</v-app-bar-title>
+  </v-app-bar>
+
   <v-main>
     <router-view />
   </v-main>
@@ -9,5 +25,10 @@
 <script lang="ts">
 export default {
   name: "DefaultLayout",
+  data() {
+    return {
+      drawer: false,
+    };
+  },
 };
 </script>
