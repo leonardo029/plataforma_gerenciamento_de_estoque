cbff1360cdc6919285a13a68dc695e8c23d78f48
leonardo029 <moreira.leonardo83@outlook.com>
2025-10-27 11:12:37 -0300

    fix: Correção da função 'findAll' definida no controller do módulo shelf

--- api/src/modules/shelf/shelf.controller.ts
+++ api/src/modules/shelf/shelf.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { ShelfService } from './shelf.service';
-import { FindAllShelfResource } from './resources';
 
 @Controller('shelf')
 export class ShelfController {
   constructor(private readonly shelfService: ShelfService) {}
 
   @Get()
-  async findAll(): Promise<FindAllShelfResource[]> {
+  findAll() {
     return this.shelfService.findAll();
   }
 }
