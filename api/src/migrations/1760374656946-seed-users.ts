ec48d4fe4287ca5eb55d31608f371e2c7b97c4ed
Leonardo Moreira <moreira.leonardo83@outlook.com>
2025-10-27 00:57:08 -0300

    fix: Adicionando senha hash válida

--- api/src/migrations/1760374656946-seed-users.ts
+++ api/src/migrations/1760374656946-seed-users.ts
@@ -4,12 +4,12 @@ export class SeedUsers1760374656946 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       INSERT INTO users (name, email, password, id_address, id_contact, is_activated, role) VALUES
-      ('André Carvalho Silva','andre.silva@example.com', '9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee6841f5c1d2d4c4e1f2d3e6a7a7b', (SELECT id FROM addresses WHERE cep = '01311000' LIMIT 1), (SELECT id FROM contacts WHERE phone_number = '98877-1122' LIMIT 1), true, 'admin'),
-      ('Fernanda Souza Lima','fernanda.lima@example.com', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', (SELECT id FROM addresses WHERE cep = '22041001' LIMIT 1), (SELECT id FROM contacts WHERE phone_number = '98765-4321' LIMIT 1), true, 'user'),
-      ('Rafael Mendes Duarte','rafael.duarte@example.com', '6cf615d5bcaac778352a8f1f3360d23d3c9d4b2a44f9c2f1819f91c2e0d7d3b2', (SELECT id FROM addresses WHERE cep = '51020001' LIMIT 1), (SELECT id FROM contacts WHERE phone_number = '99654-3210' LIMIT 1), false, 'user');
+      ('André Carvalho Silva','andre.silva@example.com', '$2a$10$vqSOk9MH6sbd8aYbr7UQZefnlw2w.JEpLJO9kX2zHtebw9W3exPOe', (SELECT id FROM addresses WHERE cep = '01311000' LIMIT 1), (SELECT id FROM contacts WHERE phone_number = '98877-1122' LIMIT 1), true, 'admin'),
+      ('Fernanda Souza Lima','fernanda.lima@example.com', '$2a$10$vqSOk9MH6sbd8aYbr7UQZefnlw2w.JEpLJO9kX2zHtebw9W3exPOe', (SELECT id FROM addresses WHERE cep = '22041001' LIMIT 1), (SELECT id FROM contacts WHERE phone_number = '98765-4321' LIMIT 1), true, 'user'),
+      ('Rafael Mendes Duarte','rafael.duarte@example.com', '$2a$10$vqSOk9MH6sbd8aYbr7UQZefnlw2w.JEpLJO9kX2zHtebw9W3exPOe', (SELECT id FROM addresses WHERE cep = '51020001' LIMIT 1), (SELECT id FROM contacts WHERE phone_number = '99654-3210' LIMIT 1), false, 'user');
     `);
   }
-
+  //senha: teste123
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
     DELETE FROM users WHERE email IN (
