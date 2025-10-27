d81402572be03150c3a2a2e79a8fef48cf4ec9ce
Leonardo Moreira <moreira.leonardo83@outlook.com>
2025-10-27 02:37:36 -0300

    feat: Centralização das informações no pinia

--- app-client/src/components/AppSnackbar.vue
+++ app-client/src/components/AppSnackbar.vue
@@ -1,14 +1,14 @@
 <template>
   <v-snackbar
-    v-model="internalModel"
-    :timeout="timeout"
-    :color="snackbarColor"
-    :location="location"
+    v-model="snackbar.show"
+    :timeout="snackbar.timeout"
+    :color="snackbar.type"
+    :location="snackbar.location"
     elevation="2"
   >
     <div class="d-flex align-center">
-      <v-icon v-if="showIcon" class="mr-2" :icon="iconName" />
-      <span>{{ message }}</span>
+      <v-icon v-if="snackbar.showIcon" class="mr-2" :icon="iconName" />
+      <span>{{ snackbar.message }}</span>
     </div>
 
     <template #actions>
@@ -17,43 +17,12 @@
   </v-snackbar>
 </template>
 
-<script>
-export default {
-  name: 'AppSnackbar',
-  props: {
-    modelValue: { type: Boolean, default: false },
-    message: { type: String, default: '' },
-    type: {
-      type: String,
-      default: 'info', // 'success' | 'error' | 'warning' | 'info'
-      validator: (v) => ['success', 'error', 'warning', 'info'].includes(v),
-    },
-    timeout: { type: Number, default: 4000 },
-    location: { type: String, default: 'bottom' }, // 'top' | 'bottom' | 'left' | 'right'
-    showIcon: { type: Boolean, default: true },
-  },
-  emits: ['update:modelValue'],
-  computed: {
-    internalModel: {
-      get() { return this.modelValue },
-      set(val) { this.$emit('update:modelValue', val) },
-    },
-    snackbarColor() {
-      return this.type
-    },
-    iconName() {
-      switch (this.type) {
-        case 'success': return 'mdi-check-circle'
-        case 'error': return 'mdi-alert-circle'
-        case 'warning': return 'mdi-alert'
-        default: return 'mdi-information'
-      }
-    },
-  },
-  methods: {
-    close() {
-      this.$emit('update:modelValue', false)
-    },
-  },
-}
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useSnackbarStore } from '@/stores/snackbar'
+
+const snackbar = useSnackbarStore()
+const iconName = computed(() => snackbar.iconName)
+
+function close() { snackbar.hide() }
 </script>
