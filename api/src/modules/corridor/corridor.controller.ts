927a4691182a86e51621d8410642c78a9258a342
Leonardo Moreira <moreira.leonardo83@outlook.com>
2025-10-27 01:00:54 -0300

    feat: Endpoint para fetch dos corredores existentes

--- api/src/modules/corridor/corridor.controller.ts
+++ api/src/modules/corridor/corridor.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CorridorService } from './corridor.service';
+import { FindAllCorridorResource } from './resources';
 
 @Controller('corridor')
-export class CorridorController {}
+export class CorridorController {
+  constructor(private readonly corridorService: CorridorService) {}
+
+  @Get()
+  async findAll(): Promise<FindAllCorridorResource[]> {
+    return this.corridorService.findAll();
+  }
+}
--- api/src/modules/corridor/corridor.module.ts
+++ api/src/modules/corridor/corridor.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CorridorEntity } from './entities';
 import { CorridorRepository } from './repositories';
 import { CorridorService } from './corridor.service';
+import { CorridorController } from './corridor.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CorridorEntity])],
-  controllers: [],
+  controllers: [CorridorController],
   providers: [CorridorService, CorridorRepository],
   exports: [CorridorService],
 })
