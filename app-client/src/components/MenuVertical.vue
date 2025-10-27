bdd9a63cb9eb3ecca0a8bcdf792e22a88c0fc1c6
leonardo029 <moreira.leonardo83@outlook.com>
2025-10-27 14:28:53 -0300

    feat: Componentização dos elementos do layout default

---
+++ app-client/src/components/MenuVertical.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-navigation-drawer app v-model="drawer" width="280">
+    <UserInfo/>
+
+    <v-divider class="my-2" />
+
+    <v-list density="comfortable" nav>
+      <v-list-item
+        to="/dashboard"
+        title="Dashboard"
+        prepend-icon="mdi-view-dashboard"
+      />
+      <v-list-item to="/stocks" title="Estoque" prepend-icon="mdi-warehouse" />
+      <v-list-item
+        to="/products"
+        title="Produtos"
+        prepend-icon="mdi-package-variant"
+      />
+      <v-list-item
+        v-if="isAdmin"
+        to="/users"
+        title="Usuários"
+        prepend-icon="mdi-account"
+      />
+      <v-divider class="my-2" />
+      <v-list-item title="Sair" prepend-icon="mdi-logout" @click="logout" />
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { useAuthStore } from "@/stores/auth";
+import { useLayoutStore } from "@/stores/layout";
+import { mapStores } from "pinia";
+export default {
+  name: "MenuVertical",
+  computed: {
+    ...mapStores(useAuthStore, useLayoutStore),
+    drawer: {
+      get() {
+        return this.layoutStore.drawer;
+      },
+      set(v: boolean) {
+        this.layoutStore.drawer = v;
+      },
+    },
+    isAdmin(): boolean {
+      return String(this.authStore.user?.role ?? "").toLowerCase() === "admin";
+    },
+  },
+  methods: {
+    logout(): void {
+      this.authStore.logoutAndRedirect();
+    },
+  },
+};
+</script>
--- app-client/src/layouts/default.vue
+++ app-client/src/layouts/default.vue
@@ -1,50 +1,6 @@
 <template>
-  <v-navigation-drawer app v-model="drawer" width="280">
-    <v-sheet class="pa-4">
-      <v-avatar class="mb-4" color="grey-darken-1" size="64">
-        <v-icon icon="mdi-account" size="40" />
-      </v-avatar>
-      <div class="text-subtitle-2">{{ userName }}</div>
-      <div class="text-caption text-medium-emphasis">{{ userEmail }}</div>
-    </v-sheet>
-
-    <v-divider class="my-2" />
-
-    <v-list density="comfortable" nav>
-      <v-list-item
-        to="/dashboard"
-        title="Dashboard"
-        prepend-icon="mdi-view-dashboard"
-      />
-      <v-list-item
-        to="/stocks"
-        title="Estoque"
-        prepend-icon="mdi-warehouse"
-      />
-      <v-list-item
-        to="/products"
-        title="Produtos"
-        prepend-icon="mdi-package-variant"
-      />
-      <v-list-item
-        v-if="isAdmin"
-        to="/users"
-        title="Usuários"
-        prepend-icon="mdi-account"
-      />
-      <v-divider class="my-2" />
-      <v-list-item
-        title="Sair"
-        prepend-icon="mdi-logout"
-        @click="logout"
-      />
-    </v-list>
-  </v-navigation-drawer>
-
-  <v-app-bar app>
-    <v-app-bar-nav-icon @click="toggleDrawer" />
-    <v-app-bar-title style="cursor: pointer" @click="goDashboard">Product Manager</v-app-bar-title>
-  </v-app-bar>
+  <MenuVertical />
+  <AppBar />
 
   <v-main>
     <router-view />
@@ -54,37 +10,7 @@
 </template>
 
 <script lang="ts">
-import { useAuthStore } from '@/stores/auth'
-import { useLayoutStore } from '@/stores/layout'
-import { mapStores } from 'pinia'
 export default {
   name: "DefaultLayout",
-  computed: {
-    ...mapStores(useAuthStore, useLayoutStore),
-    drawer: {
-      get() { return this.layoutStore.drawer },
-      set(v: boolean) { this.layoutStore.drawer = v },
-    },
-    userName(): string {
-      return this.authStore.user?.name ?? 'Usuário'
-    },
-    userEmail(): string {
-      return this.authStore.user?.email ?? ''
-    },
-    isAdmin(): boolean {
-      return String(this.authStore.user?.role ?? '').toLowerCase() === 'admin'
-    }
-  },
-  methods: {
-    toggleDrawer(): void {
-      this.layoutStore.toggleDrawer()
-    },
-    goDashboard(): void {
-      this.layoutStore.goDashboard()
-    },
-    logout(): void {
-      this.authStore.logoutAndRedirect()
-    }
-  }
 };
 </script>
