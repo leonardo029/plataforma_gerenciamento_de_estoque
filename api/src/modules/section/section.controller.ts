35bf08b0c0fe51e86ee56793f1a33b092fc16bae
leonardo029 <moreira.leonardo83@outlook.com>
2025-10-27 11:09:34 -0300

    fix: Correção da função 'findAll' definida no controller do módulo section

--- api/src/modules/section/section.controller.ts
+++ api/src/modules/section/section.controller.ts
@@ -1,13 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { SectionService } from './section.service';
-import { FindAllSectionResource } from './resources';
 
 @Controller('section')
 export class SectionController {
   constructor(private readonly sectionService: SectionService) {}
 
   @Get()
-  async findAll(): Promise<FindAllSectionResource[]> {
+  findAll() {
     return this.sectionService.findAll();
   }
 }
