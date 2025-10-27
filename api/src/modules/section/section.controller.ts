151d88e518a93db2727a59238fd43a8e420cd6db
Leonardo Moreira <moreira.leonardo83@outlook.com>
2025-10-27 01:01:20 -0300

    feat: Endpoint para fetch das seções existentes

--- api/src/modules/section/section.controller.ts
+++ api/src/modules/section/section.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { SectionService } from './section.service';
+import { FindAllSectionResource } from './resources';
 
 @Controller('section')
-export class SectionController {}
+export class SectionController {
+  constructor(private readonly sectionService: SectionService) {}
+
+  @Get()
+  async findAll(): Promise<FindAllSectionResource[]> {
+    return this.sectionService.findAll();
+  }
+}
